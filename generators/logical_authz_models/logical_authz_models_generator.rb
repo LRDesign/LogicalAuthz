e7f48bf0c51223dce5a76b8297b4ab6c3b2b4756
Judson <nyarly@gmail.com>
2010-04-08 18:04:55 -0700

    Uses generators for models

--- generators/logical_authz_models/logical_authz_models_generator.rb
+++ generators/logical_authz_models/logical_authz_models_generator.rb
@@ -1,11 +1,10 @@
-module LogicalAuthz
-  class ModelsGenerator < Rails::Generators::Base
+class LogicalAuthzModelsGenerator < Rails::Generator::Base
+  def manifest 
     record do |manifest|
       manifest.class_collisions "Group", "Permission"
       manifest.file "app/models/group.rb", "app/models/group.rb", :collision => :skip
       manifest.file "app/models/permission.rb", "app/models/permission.rb", :collision => :skip
-      manifest.migration_file "setup_logical_authz.rb", "db/migrate"
+      manifest.migration_template "setup_logical_authz.rb", "db/migrate", :migration_file_name => "setup_logical_authz"
     end
   end
 end
-
