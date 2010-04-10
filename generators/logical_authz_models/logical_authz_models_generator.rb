dbbb4c1fcd984a5dacd732a9d489b0ec06e83b39
Judson <nyarly@gmail.com>
2010-04-09 18:23:49 -0700

    Generator fixes

--- generators/logical_authz_models/logical_authz_models_generator.rb
+++ generators/logical_authz_models/logical_authz_models_generator.rb
@@ -32,7 +32,9 @@ class LogicalAuthzModelsGenerator < Rails::Generator::Base
       manifest.template "config/initializers/logical_authz.rb.erb", "config/initializers/logical_authz.rb", :assigns => template_data
       manifest.template "db/seeds_logical_authz.rb.erb", "db/seeds_logical_authz.rb", :assigns => template_data
       manifest.migration_template "migrations/create_groups.rb.erb", "db/migrate", :migration_file_name => "create_groups", :assigns => template_data
+      sleep(1) #I have some not nice things to say about Rails::Generator
       manifest.migration_template "migrations/create_permissions.rb.erb", "db/migrate", :migration_file_name => "create_permissions", :assigns => template_data
+      sleep(1)
       manifest.migration_template "migrations/create_users_groups.rb.erb", "db/migrate", :migration_file_name => "create_users_groups", :assigns => template_data
     end
   end
