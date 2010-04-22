85a18dc3fdff98ebeb21830069181a661cd39968
Judson <nyarly@gmail.com>
2010-04-21 19:45:21 -0700

    Fixing templatings

--- generators/logical_authz_specs/logical_authz_specs_generator.rb
+++ generators/logical_authz_specs/logical_authz_specs_generator.rb
@@ -15,10 +15,10 @@ class LogicalAuthzSpecsGenerator < LogicalAuthz::Generator
         templ.template "spec/factories/az_groups.rb.erb", "spec/factories/logical_authz_#{template_data[:group_table]}.rb"
         templ.template "spec/support/logical_authz.rb.erb", "spec/support/logical_authz.rb"
         templ.template "spec/support/mock_auth.rb.erb", "spec/support/mock_auth.rb"
-        templ.template "spec/controllers/permissions_controller_spec.rb.erb"
-        templ.template "spec/controllers/groups_controller_spec.rb.erb"
-        templ.template "spec/controllers/groups_users_controller_spec.rb.erb"
-        templ.template "spec/helpers/logical_authz_helper_spec.rb.erb"
+        templ.template "spec/controllers/permissions_controller_spec.rb.erb", "spec/controllers/permissions_controller_spec.rb"
+        templ.template "spec/controllers/groups_controller_spec.rb.erb", "spec/controllers/groups_controller_spec.rb"
+        templ.template "spec/controllers/groups_users_controller_spec.rb.erb", "spec/controllers/groups_users_controller_spec.rb"
+        templ.template "spec/helpers/logical_authz_helper_spec.rb.erb", "spec/helpers/logical_authz_helper_spec.rb"
       end
     end
   end
